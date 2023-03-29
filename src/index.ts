c726457aaa002fb82f15263630b2ed65acb2d94d
Jonas Birmé <jonas.birme@eyevinn.se>
2023-03-29 13:47:34 +0200

    feat: display channel name and debug info in full screen

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const params = new URLSearchParams(queryString);
 
 const engineHost = params.get('engine') || 'https://fast.vc.eyevinn.technology';
 
+interface Endpoint {
+  playback: string | URL;
+  health: string;
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
   const sectionMultiview = document.querySelector('#multiview');
   const inputEngineHost =
@@ -29,24 +34,42 @@ window.addEventListener('DOMContentLoaded', async () => {
       if (response.ok) {
         const json = await response.json();
         if (json.sessionEndpoints) {
-          json.sessionEndpoints.forEach(
-            (endpoint: { playback: string | URL }) => {
-              const video = document.createElement('eyevinn-video');
-              video.setAttribute('muted', '');
-              video.setAttribute('autoplay', '');
-              video.setAttribute('autoplay-visible', '');
-              video.setAttribute(
-                'source',
-                new URL(endpoint.playback, inputEngineHost.value).toString()
-              );
-              sectionMultiview?.appendChild(video);
-            }
-          );
+          json.sessionEndpoints.forEach((endpoint: Endpoint) => {
+            const container = document.createElement('div');
+            container.className = 'container';
+            const video = document.createElement('eyevinn-video');
+            video.setAttribute('muted', '');
+            video.setAttribute('autoplay', '');
+            video.setAttribute('autoplay-visible', '');
+            video.setAttribute(
+              'source',
+              new URL(endpoint.playback, inputEngineHost.value).toString()
+            );
+            container.appendChild(video);
+            sectionMultiview?.appendChild(container);
+
+            const healthUrl = new URL(endpoint.health, inputEngineHost.value);
+
+            const divChannelInfo = document.createElement('div');
+            const channelId = healthUrl.pathname.split('/').pop();
+            divChannelInfo.className = 'channel-name-overlay';
+            divChannelInfo.dataset.channelId = channelId;
+            divChannelInfo.innerHTML = `${channelId}`;
+            container.appendChild(divChannelInfo);
+
+            const divDebugInfo = document.createElement('div');
+            divDebugInfo.dataset.channelId = channelId;
+            divDebugInfo.dataset.healthUrl = healthUrl.toString();
+            divDebugInfo.className = 'debug-overlay';
+            container.appendChild(divDebugInfo);
+          });
           const r = document.querySelector<HTMLElement>(':root');
           const columns = calculateColumns(json.sessionEndpoints.length);
           r?.style.setProperty('--fullscreen-column-count', columns.toString());
         }
         updateThisUrl();
+        await updateDebugInfo();
+        setInterval(updateDebugInfo, 5000);
       }
     }
   });
@@ -64,5 +87,22 @@ window.addEventListener('DOMContentLoaded', async () => {
         thisUrl.value = url.toString();
       }
     }
+  }
+
+  async function updateDebugInfo() {
+    const debugInfos =
+      document.querySelectorAll<HTMLDivElement>('.debug-overlay');
+    debugInfos.forEach(async (debugInfo) => {
+      const healthUrl = debugInfo.dataset.healthUrl;
+      if (healthUrl) {
+        const response = await fetch(healthUrl);
+        if (response.ok) {
+          const json = await response.json();
+          debugInfo.innerHTML =
+            `<div>TICK: ${json.tick}</div>` +
+            `<div>MSEQ: ${json.mediaSeq}</div>`;
+        }
+      }
+    });
   }
 });
